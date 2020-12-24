7cf58720a19f32709f604f6451d7c23d29e31e17
Clovis Picoli <clovispicoli@gmail.com>
2020-12-24 18:38:09 -0300

    DELETE new client

--- backend/src/main/java/com/projectcli/dsclient/services/ClientService.java
+++ backend/src/main/java/com/projectcli/dsclient/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.projectcli.dsclient.dto.ClientDTO;
 import com.projectcli.dsclient.entities.Client;
 import com.projectcli.dsclient.repositories.ClientRepository;
+import com.projectcli.dsclient.services.exceptions.DatabaseException;
 import com.projectcli.dsclient.services.exceptions.ResourceNotFoundException;
 
 
@@ -61,5 +64,18 @@ public class ClientService {
  		catch (EntityNotFoundException e) {
  			throw new ResourceNotFoundException("Id not found " + id);
  		}
+ 	}
+	
+	public void delete(Long id) {
+ 		try {
+ 		repository.deleteById(id);
+ 		}
+ 		catch(EmptyResultDataAccessException e) {
+ 			throw new ResourceNotFoundException("Id not found " + id);
+ 		}
+ 		catch(DataIntegrityViolationException e) {
+ 			throw new DatabaseException("Integrity violation");
+ 		}
+
  	}
 }
