8a68a25199f2b2a23a3df90ca6871952f2d43b9a
Clovis Picoli <clovispicoli@gmail.com>
2020-12-26 06:57:06 -0300

    Auditing data

--- backend/src/main/java/com/projectcli/dsclient/entities/Client.java
+++ backend/src/main/java/com/projectcli/dsclient/entities/Client.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +28,12 @@ public class Client implements Serializable {
 	
 	@Column(name = "birthDate")
 	private Instant birthDate;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Client() {
 	}
@@ -78,6 +86,23 @@ public class Client implements Serializable {
 		this.birthDate = birthdate;
 	}
 
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_client (name, cpf, email, birth_date) VALUES ('MARIA SILVA', '12345678901', 'maria@gmail.com', TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z');
-INSERT INTO tb_client (name, cpf, email, birth_date) VALUES ('João silveira', '098765432123', 'joao@gmail.com', TIMESTAMP WITH TIME ZONE '1992-08-20T10:30:00Z');
+INSERT INTO tb_client (name, cpf, email, birth_date, created_At) VALUES ('MARIA SILVA', '12345678901', 'maria@gmail.com', TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', NOW());
+INSERT INTO tb_client (name, cpf, email, birth_date, created_At) VALUES ('João silveira', '098765432123', 'joao@gmail.com', TIMESTAMP WITH TIME ZONE '1992-08-20T10:30:00Z', NOW());
